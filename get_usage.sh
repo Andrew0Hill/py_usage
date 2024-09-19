f58a2d204fa667e8a3346f3b6b96297838be07b2
Andrew Hill <andrew.2.hill@cuanschutz.edu>
2024-09-19 13:22:32 -0600

    Fixed issue when scripts were called outside of working directory

--- get_usage.sh
+++ get_usage.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+
+# Default output path is 'job_stats.txt'.
 OUTPUT_PATH=${1:-"job_stats.txt"}
+
+# Default interval is 5 seconds.
 INTERVAL=${2:-5}
 
+# Activate virtual environment
 source $HOME/vm_env/bin/activate
 
-python get_usage.py --output_file=$OUTPUT_PATH --interval=$INTERVAL
+# We may not be executing from the working directory, so figure out where we are.
+SCRIPT_DIR=$(dirname -- "$0")
+SCRIPT_DIR_PATH=$(realpath "$SCRIPT_DIR")
+
+# Run the statistics script.
+python "$SCRIPT_DIR_PATH/get_usage.py" --output_file=$OUTPUT_PATH --interval=$INTERVAL
--- make_usage_plot.sh
+++ make_usage_plot.sh
@@ -6,7 +6,10 @@ INPUT_PREFIX=${1:-"job_stats"}
 # Activate virtual environment
 source $HOME/vm_env/bin/activate
 
+SCRIPT_DIR=$(dirname -- "$0")
+SCRIPT_DIR_PATH=$(realpath "$SCRIPT_DIR")
+
 # Run the plotting script.
 # The --stats line determines which outputs are included in the plot.
-python3 make_usage_plot.sh --input_prefix="$INPUT_PREFIX" \
-                           --stats mem cpu disk \
+python3 "$SCRIPT_DIR_PATH/make_usage_plot.py" --input_prefix="$INPUT_PREFIX" \
+                                              --stats mem cpu disk \
